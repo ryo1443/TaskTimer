b27f7a0ca6efce307aab94d94a91ead7b4123b81
ryo <ryo12271443@icloud.com>
2020-08-07 01:25:31 +0900

    進捗良

--- app/src/main/java/com/e/tasktimer/MainActivity.kt
+++ app/src/main/java/com/e/tasktimer/MainActivity.kt
@@ -1,26 +1,67 @@
 package com.e.tasktimer
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.content_main.*
 
 private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity(), AddEditFragment.OnSaveClicked {
+class MainActivity() : AppCompatActivity(), AddEditFragment.OnSaveClicked {
+
+    //Activityが2-pane mode→ landscape or tablet
+    //mTwoPaneというプロパティでそれを表現する
+    private var mTwoPane = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        mTwoPane = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
+        var fragment = supportFragmentManager.findFragmentById(R.id.task_detail_contaier)
+        if (fragment != null) {
+            showEditPane()
+        } else {
+            task_detail_contaier.visibility = if (mTwoPane) View.INVISIBLE else View.GONE
+            mainFragment.view?.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showEditPane() {
+        //修正中のタスクがあったか、正しくpaneがセットされているか確認する
+        task_detail_contaier.visibility = View.VISIBLE
+        //もしsingle paneなら左のpaneを隠す
+        mainFragment.view?.visibility = if (mTwoPane) View.VISIBLE else View.GONE
+    }
+
+    private fun removeEditPane(fragment: Fragment? = null) {
+        Log.d(TAG, "removeEditPane called")
+
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .remove(fragment)
+                .commit()
+        }
+
+        //set the visibility of the right hand pane（右のフラグメント）
+        task_detail_contaier.visibility = if (mTwoPane) View.INVISIBLE else View.GONE
+        //and show the left hand pane（左のフラグメント）
+        mainFragment.view?.visibility = View.VISIBLE
     }
 
     override fun onSaveClicked() {
-        TODO("Not yet implemented")
+        Log.d(TAG, "onSaveClicked called")
+        var fragment = supportFragmentManager.findFragmentById(R.id.task_detail_contaier)
+        removeEditPane(fragment)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -44,13 +85,14 @@ class MainActivity : AppCompatActivity(), AddEditFragment.OnSaveClicked {
         Log.d(TAG, "taskEditRequest: starts")
 
         //create a new fragment to edit the task
-        val newFragment = task?.let { AddEditFragment.newInstance(it) }
-        if (newFragment != null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id,fragment, newFragment)
-                .commit()
-        }
-        Log.d(TAG, "Existing taskEditRequest")
+        val newFragment = AddEditFragment.newInstance(task)
+        Log.d(TAG, "check")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.task_detail_contaier, newFragment)
+            .commit()
 
+        showEditPane()
+
+        Log.d(TAG, "Existing taskEditRequest")
     }
 }
